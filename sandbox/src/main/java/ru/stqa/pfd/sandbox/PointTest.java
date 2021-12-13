4b2e59295c48fc793dc10c945a6f72f0613088c3
MSinkova <msinkova92@gmail.com>
2021-12-13 16:30:59 +0300

    Выполнено второе задание

--- sandbox/src/main/java/ru/stqa/pfd/sandbox/PointTest.java
+++ sandbox/src/main/java/ru/stqa/pfd/sandbox/PointTest.java
@@ -7,7 +7,6 @@ public class PointTest {
 
         Point p1 = new Point(22, 5);
         Point p2 = new Point(11, 98);
-
         System.out.println( "Расстояние между точками = " + p1.distance(p2));
     }
 }
