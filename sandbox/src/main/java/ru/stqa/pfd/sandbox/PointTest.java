53a37187443f46bee6ebbac8218823056bb8a7f2
MSinkova <msinkova92@gmail.com>
2021-12-13 15:55:23 +0300

    Функцию переделала в метод

--- sandbox/src/main/java/ru/stqa/pfd/sandbox/PointTest.java
+++ sandbox/src/main/java/ru/stqa/pfd/sandbox/PointTest.java
@@ -1,18 +1,13 @@
+package ru.stqa.pfd.sandbox;
+
+import ru.stqa.pfd.sandbox.Point;
+
 public class PointTest {
     public static void main (String[] arg) {
 
-        Point p1 = new Point();
-        Point p2 = new Point();
+        Point p1 = new Point(22, 5);
+        Point p2 = new Point(11, 98);
 
-        p1.x = 12;
-        p1.y = 22;
-        p2.x = 9;
-        p2.y = 34;
-
-        System.out.println( "Расстояние между точками = " + distance(p1, p2));
-    }
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+        System.out.println( "Расстояние между точками = " + p1.distance(p2));
     }
 }
