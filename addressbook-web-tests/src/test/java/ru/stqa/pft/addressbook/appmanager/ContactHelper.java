df99f980f2b43b3522b51815dd8d69459926bb42
MSinkova <msinkova92@gmail.com>
2021-12-30 22:10:35 +0300

    Создан базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,66 +4,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-    private FirefoxDriver wd;
+public class ContactHelper extends HelperBase {
+
 
     public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+      super(wd);
     }
 
     public void submitContactCreation() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+      click(By.xpath("//div[@id='content']/form/input[21]"));
     }
 
     public void fillContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.firstname());
-      wd.findElement(By.name("middlename")).click();
-      wd.findElement(By.name("middlename")).clear();
-      wd.findElement(By.name("middlename")).sendKeys(contactData.middlename());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.lastname());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contactData.home());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.mobile());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.email());
-      wd.findElement(By.name("email2")).click();
-      wd.findElement(By.name("email2")).clear();
-      wd.findElement(By.name("email2")).sendKeys(contactData.email2());
-      wd.findElement(By.name("bday")).click();
-      wd.findElement(By.name("bmonth")).click();
-      wd.findElement(By.name("byear")).click();
-      wd.findElement(By.name("byear")).clear();
-      wd.findElement(By.name("byear")).sendKeys(contactData.byear());
-      wd.findElement(By.name("address2")).click();
-      wd.findElement(By.name("address2")).clear();
-      wd.findElement(By.name("address2")).sendKeys(contactData.address2());
-      wd.findElement(By.name("phone2")).click();
-      wd.findElement(By.name("phone2")).clear();
-      wd.findElement(By.name("phone2")).sendKeys(contactData.phone2());
+      type(By.name("firstname"),contactData.firstname());
+      type(By.name("middlename"),contactData.middlename());
+      type(By.name("lastname"),contactData.lastname());
+      type(By.name("home"),contactData.home());
+      type(By.name("mobile"),contactData.mobile());
+      type(By.name("mobile"),contactData.mobile());
+      type(By.name("email"),contactData.email());
+      type(By.name("email2"),contactData.email2());
+      click(By.name("bday"));
+      click(By.name("bmonth"));
+      type(By.name("byear"),contactData.byear());
+      type(By.name("address2"),contactData.address2());
+      type(By.name("phone2"),contactData.phone2());
     }
 
     public void initContactCreation() {
-      wd.findElement(By.linkText("add new")).click();
+      click(By.linkText("add new"));
     }
 
     public void goToContactPage() {
-      wd.findElement(By.id("logo")).click();
+      click(By.id("logo"));
     }
 
     public void deleteSelectedContacts() {
-        wd.findElement(By.xpath("//input[@value='Delete']")).click();
-        wd.switchTo().alert().accept();
+      click(By.xpath("//input[@value='Delete']"));
+      wd.switchTo().alert().accept();
     }
 
     public void selectContact() {
-        wd.findElement(By.id("MassCB")).click();
+      click(By.id("MassCB"));
     }
 }
