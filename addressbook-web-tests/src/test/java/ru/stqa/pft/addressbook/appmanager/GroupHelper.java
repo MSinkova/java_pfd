adbfe641a3214903a14d845b3b18febd24c0186b
MSinkova <msinkova92@gmail.com>
2022-01-17 18:25:14 +0300

    4.4. Выбор последней группы из списка при изменении и удалении

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -29,8 +29,8 @@ public class GroupHelper extends HelperBase {
       click(By.name("new"));
     }
 
-    public void selectGroup() {
-      click(By.name("selected[]"));
+    public void selectGroup(int index) {
+        wd.findElements(By.name("selected[]")).get(index).click(); //находтм все элементы по локатору Selected[]
     }
 
     public void deleteSelectedGroups() {
