5588312a05fe59c2b7377520917666c795add7df
MSinkova <msinkova92@gmail.com>
2022-01-30 20:08:16 +0300

    6.1 Добавление файла по абсолютному пути

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,11 +15,22 @@ public class ContactAddTest extends ContactTestBase {
   public void testContactAdd() {
     app.contact().contactPage();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withLastname("Petrov").withFirstname("Ivan").withMobilePhone("78961234545");
+    File photo = new File("src/test/resources/file.png");
+
+    ContactData contact = new ContactData().withLastname("Petrov").withFirstname("Ivan").withMobilePhone("78961234545").withPhoto(photo);
     create(contact);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt(g -> g.getId()).max().getAsInt()))));
   }
+
+ // @Test
+ // public void testCurrentDir() {
+ //   File currentDir = new File(".");
+ //   System.out.println(currentDir.getAbsolutePath());
+ //   File photo = new File("src/test/resources/file.png");
+ //   System.out.println(photo.getAbsolutePath());
+ //   System.out.println(photo.exists());
+ // }
 }
