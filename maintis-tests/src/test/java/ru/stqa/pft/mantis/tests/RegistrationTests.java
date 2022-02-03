dac030867edbdb78c35f91d064fdfa3c2d9ace7c
MSinkova <msinkova92@gmail.com>
2022-02-03 23:28:17 +0300

    Передача файлов на удалённую машину по протоколу FTP

--- maintis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ maintis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -2,11 +2,19 @@ package ru.stqa.pft.mantis.tests;
 
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class RegistrationTests extends TestBase{
 
     @Test
     public void testRegistration() {
         app.registration().start("user1", "user1@localhost.localdomain");
+    }
+
+    @Test
+    public void testCurrentDir() {
+        File currentDir = new File(".");
+        System.out.println(currentDir.getAbsolutePath() + " рабочая директория");
     }
 
 }
